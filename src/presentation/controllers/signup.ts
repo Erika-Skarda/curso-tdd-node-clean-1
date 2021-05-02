8c499a84cf02c3aa388cea7d1deb700fd41096f2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2021-05-02 20:53:54 -0300

    feat: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,15 @@
+/**
+ * Arquivo: src/presentation/controllers/signup.ts
+ * Data: 02/05/2021
+ * Descrição: arquivo responsável pela classe em TypeScript de 'Signup'
+ * Autora: Glaucia Lemos
+ * Twitter: <@glaucia_lemos86>
+ */
+
+export class SignUpController {
+  handle (httpRequest: any): any {
+    return {
+      statusCode: 400
+    }
+  }
+}
